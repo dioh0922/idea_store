ea975f06584111bc371845b7f3add0d3cf209936
dioh0922 <diohound@gmail.com>
2022-01-05 10:39:39 +0900

    登録成功の挙動まで

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
+
 let p = document.createElement("p");
 let input = document.createElement("input");
 let button = document.createElement("button");
-button.textContent = "保存する";
+button.textContent = "登録する";
 
 button.addEventListener("click", () => {
+	p.textContent = "登録しています";
 	const httpRequest = new XMLHttpRequest();
 	httpRequest.open("POST", "./backend/saveIdea.php", true);
 	httpRequest.onreadystatechange = () => {
 		if(httpRequest.readyState === 4 && httpRequest.status == 200){
 			const jsonText = httpRequest.responseText;
 			const json = JSON.parse(jsonText);
-			console.log(json);
+			if(json.result == 1){
+				location.reload();
+			}else{
+				p.textContent = "登録に失敗しました";
+			}
 		}
 	}
-	let post_data = {text: input.value};
-	httpRequest.send(JSON.stringify(post_data));
+	const post_data: FormData = new FormData();
+	post_data.append("text", input.value);
+	httpRequest.send(post_data);
 });
 
 const app = document.getElementById("app");
